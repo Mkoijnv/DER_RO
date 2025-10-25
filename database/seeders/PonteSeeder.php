c283f3871fc9067a0d877de912c33be92b912f2c
Vinicius <viniciusrico17@gmail.com>
2025-10-25 14:18:47 -0400

    feat: Atualizar seeders de Pontes e Rodovias com novos dados e melhorias na estrutura

--- database/seeders/PonteSeeder.php
+++ database/seeders/PonteSeeder.php
@@ -11,58 +11,124 @@ class PonteSeeder extends Seeder
     {
         $pontes = [
             [
-                'nome' => 'Ponte do Abunã',
                 'rodovia_id' => 1,
-                'rio' => 'Rio Abunã',
-                'km' => 5.2,
-                'latitude' => -10.95,
-                'longitude' => -62.85,
-                'material' => 'concreto',
-                'situacao' => 'boa',
-                'foto' => null
-            ],
-            [
-                'nome' => 'Ponte sobre o Rio Urupá',
-                'rodovia_id' => 3,
-                'rio' => 'Rio Urupá',
-                'km' => 3.7,
-                'latitude' => -11.0,
-                'longitude' => -62.9,
-                'material' => 'aço',
-                'situacao' => 'regular',
-                'foto' => null
-            ],
-            [
-                'nome' => 'Ponte sobre o Rio Jamari',
-                'rodovia_id' => 2,
-                'rio' => 'Rio Jamari',
-                'km' => 4.1,
-                'latitude' => -11.1,
-                'longitude' => -62.95,
-                'material' => 'misto',
-                'situacao' => 'boa',
-                'foto' => null
-            ],
-            [
-                'nome' => 'Ponte sobre o Rio Pardo',
-                'rodovia_id' => 3,
-                'rio' => 'Rio Pardo',
-                'km' => 6.5,
-                'latitude' => -11.2,
-                'longitude' => -62.88,
-                'material' => 'madeira',
-                'situacao' => 'ruim',
-                'foto' => null
-            ],
-            [
-                'nome' => 'Ponte sobre o Rio Madeira',
-                'rodovia_id' => 1,
+                'nome' => 'Ponte Rondon-Roosevelt',
                 'rio' => 'Rio Madeira',
-                'km' => 8.3,
-                'latitude' => -10.9,
-                'longitude' => -63.0,
+                'km' => 976.00,
+                'latitude' => -8.7377497,
+                'longitude' => -63.9223130,
                 'material' => 'concreto',
                 'situacao' => 'boa',
+                'foto' => null
+            ],
+            [
+                'rodovia_id' => 2,
+                'nome' => 'Ponte do Abunã',
+                'rio' => 'Rio Madeira',
+                'km' => 60.50,
+                'latitude' => -9.6660559,
+                'longitude' => -65.4454925,
+                'material' => 'concreto',
+                'situacao' => 'boa',
+                'foto' => null
+            ],
+            [
+                'rodovia_id' => 3,
+                'nome' => 'Ponte sobre o Rio Candeias',
+                'rio' => 'Rio Candeias',
+                'km' => 693.00,
+                'latitude' => -8.7991914,
+                'longitude' => -63.7171922,
+                'material' => 'concreto',
+                'situacao' => 'boa',
+                'foto' => null
+            ],
+            [
+                'rodovia_id' => 4,
+                'nome' => 'Viaduto da Jatuarana',
+                'rio' => 'Terrestre (sobreposição viária)',
+                'km' => 0.00,
+                'latitude' => -8.7778780,
+                'longitude' => -63.8719981,
+                'material' => 'concreto',
+                'situacao' => 'boa',
+                'foto' => null
+            ],
+            [
+                'rodovia_id' => 5,
+                'nome' => 'Viaduto do Trevo do Roque',
+                'rio' => 'Terrestre (sobreposição viária)',
+                'km' => 0.00,
+                'latitude' => -8.7713176,
+                'longitude' => -63.8831739,
+                'material' => 'concreto',
+                'situacao' => 'boa',
+                'foto' => null
+            ],
+            [
+                'rodovia_id' => 6,
+                'nome' => 'Viaduto da Campos Sales',
+                'rio' => 'Terrestre (sobreposição viária)',
+                'km' => 0.00,
+                'latitude' => -8.7842772,
+                'longitude' => -63.8967911,
+                'material' => 'concreto',
+                'situacao' => 'boa',
+                'foto' => null
+            ],
+            [
+                'rodovia_id' => 7,
+                'nome' => 'Alça de Acesso à Zona Sul',
+                'rio' => 'Terrestre (acesso)',
+                'km' => 0.00,
+                'latitude' => -8.7850512,
+                'longitude' => -63.8968769,
+                'material' => 'concreto',
+                'situacao' => 'boa',
+                'foto' => null
+            ],
+            [
+                'rodovia_id' => 8,
+                'nome' => 'Ponte na Estrada do Areia Branca',
+                'rio' => 'Igarapé Bate Estacas',
+                'km' => 0.50,
+                'latitude' => -8.7978228,
+                'longitude' => -63.9014190,
+                'material' => 'concreto',
+                'situacao' => 'boa',
+                'foto' => null
+            ],
+            [
+                'rodovia_id' => 9,
+                'nome' => 'Ponte na Rua 16 de Junho',
+                'rio' => 'Igarapé Urbano',
+                'km' => 0.10,
+                'latitude' => -10.0000000,
+                'longitude' => -64.0000000,
+                'material' => 'concreto',
+                'situacao' => 'boa',
+                'foto' => null
+            ],
+            [
+                'rodovia_id' => 10,
+                'nome' => 'Desvio Duplo de Igarapé',
+                'rio' => 'Igarapé',
+                'km' => 0.20,
+                'latitude' => -10.0000000,
+                'longitude' => -64.0000000,
+                'material' => 'concreto',
+                'situacao' => 'boa',
+                'foto' => null
+            ],
+            [
+                'rodovia_id' => 11,
+                'nome' => 'Pontes Ferroviárias (diversas) - EFMM',
+                'rio' => 'Vários igarapés',
+                'km' => 0.00,
+                'latitude' => -10.0000000,
+                'longitude' => -64.0000000,
+                'material' => 'aço',
+                'situacao' => 'interditada',
                 'foto' => null
             ],
         ];
@@ -70,5 +136,7 @@ class PonteSeeder extends Seeder
         foreach ($pontes as $ponte) {
             Ponte::create($ponte);
         }
+        
+        $this->command->info('✅ 11 pontes de Rondônia criadas com sucesso!');
     }
 }
--- database/seeders/RodoviaSeeder.php
+++ database/seeders/RodoviaSeeder.php
@@ -9,31 +9,111 @@ class RodoviaSeeder extends Seeder
 {
     public function run()
     {
-        // IMPORTANTE: Rondônia sempre será ID 22
-        $estado_id_rondonia = 22;
-        
-        // Verificar se o estado existe
-        $rondonia = \App\Models\Estado::find($estado_id_rondonia);
-        
-        if (!$rondonia || $rondonia->sigla !== 'RO') {
-            $this->command->error('Estado de Rondônia (ID 22) não encontrado! Execute o EstadoSeeder primeiro.');
-            return;
-        }
-
         $rodovias = [
-            ['nome' => 'BR-364', 'estado_id' => 22],
-            ['nome' => 'BR-421', 'estado_id' => 22],
-            ['nome' => 'RO-010', 'estado_id' => 22],
-            ['nome' => 'RO-135', 'estado_id' => 22],
-            ['nome' => 'RO-370', 'estado_id' => 22],
-            ['nome' => 'RO-491', 'estado_id' => 22],
-            ['nome' => 'RO-492', 'estado_id' => 22],
+            [
+                'nome' => 'BR-319',
+                'trecho_inicial' => 'Porto Velho (RO)',
+                'trecho_final' => 'Humaitá (AM) / fronteira Norte',
+                'extensao_km' => 975.00,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'BR-364',
+                'trecho_inicial' => 'Entrocamento com BR-429 (sul/RO)',
+                'trecho_final' => 'Entrocamento com BR-319 (Porto Velho)',
+                'extensao_km' => 686.70,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'RO-133',
+                'trecho_inicial' => 'Ouro Preto do Oeste',
+                'trecho_final' => 'Distrito de Rondominas',
+                'extensao_km' => 45.30,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'BR-364 (Av. Jatuarana)',
+                'trecho_inicial' => 'BR-364 entroncamento',
+                'trecho_final' => 'Av. Jatuarana - Porto Velho',
+                'extensao_km' => 2.50,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'BR-364 (Trevo do Roque)',
+                'trecho_inicial' => 'Trevo do Roque',
+                'trecho_final' => 'Trevo do Roque - arredores',
+                'extensao_km' => 1.20,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'BR-364 (Rua Campos Sales)',
+                'trecho_inicial' => 'Rua Campos Sales',
+                'trecho_final' => 'Trecho urbano - Campos Sales',
+                'extensao_km' => 0.80,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'BR-364 (Alça Zona Sul)',
+                'trecho_inicial' => 'BR-364 (Rua Três e Meio)',
+                'trecho_final' => 'Acesso Zona Sul',
+                'extensao_km' => 1.50,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'Estrada do Areia Branca',
+                'trecho_inicial' => 'Zona Sul - ponto inicial',
+                'trecho_final' => 'Zona Sul - ponto final',
+                'extensao_km' => 3.20,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'Rua 16 de Junho (via municipal)',
+                'trecho_inicial' => 'Início urbano',
+                'trecho_final' => 'Término urbano',
+                'extensao_km' => 1.10,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'Estrada do Santo Antônio',
+                'trecho_inicial' => 'Trecho inicial',
+                'trecho_final' => 'Trecho final',
+                'extensao_km' => 4.00,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'Estrada de Ferro Madeira-Mamoré (EFMM) - trechos',
+                'trecho_inicial' => 'Diversos trechos',
+                'trecho_final' => 'Diversos trechos',
+                'extensao_km' => 50.00,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'RO-010',
+                'trecho_inicial' => 'Pimenta Bueno / Nova Estrela',
+                'trecho_final' => 'Rolim de Moura / Nova Estrela',
+                'extensao_km' => 120.00,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'RO-135',
+                'trecho_inicial' => 'Trecho inicial (ex.)',
+                'trecho_final' => 'Trecho final (ex.)',
+                'extensao_km' => 78.50,
+                'situacao' => 'regular',
+            ],
+            [
+                'nome' => 'RO-370',
+                'trecho_inicial' => 'Trevo Colorado / Cabixi',
+                'trecho_final' => 'Corumbiara / Trevo Corumbiara',
+                'extensao_km' => 200.00,
+                'situacao' => 'regular',
+            ],
         ];
 
         foreach ($rodovias as $rodovia) {
             Rodovia::create($rodovia);
         }
         
-        $this->command->info('✅ 7 rodovias de Rondônia criadas com estado_id = 22');
+        $this->command->info('✅ 14 rodovias de Rondônia criadas com sucesso!');
     }
 }
